151c367fe7478b2fbe9dcb78f698d6e2c974a492
Jon Cooper <joncoop1972@gmail.com>
2017-08-30 09:44:14 -0400

    Initial sharing of project

---
+++ Geometry.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Geometry here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Geometry 
+{
+    
+    public static double triangleArea(double base, double height) 
+    {
+        double area = base * height / 2.0;
+        
+        return area;
+    }
+    
+    public static double rectangleArea(double length, double width) 
+    {
+        return length * width;
+    }
+    
+    public static double sphereVolume(double radius) 
+    {
+        return 4.0 / 3.0 * Math.PI * Math.pow(radius, 3);
+    }
+    
+    public static void main(String[] args) 
+    {
+        double ta1 = triangleArea(4.2, 8.1);
+        double ta2 = triangleArea(3.0, 123.157);
+        
+        System.out.println(ta1);
+        System.out.println(ta2);
+        
+        double ra1 = rectangleArea(5.2, 9.3);
+        System.out.println(ra1);
+    }
+}
