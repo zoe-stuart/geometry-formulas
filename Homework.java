7e65bef6da91bc21c2940f0ad2a5ecf989876d48
Jon Cooper <joncoop@users.noreply.github.com>
2017-08-31 07:23:39 -0400

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Homework here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Homework
+{
+    public static void main(String[] aaaaaaarrrrrrrrgggg)
+    {
+        System.out.println( Geometry.areaOfParallelogram(5, 4) );
+    }
+}
